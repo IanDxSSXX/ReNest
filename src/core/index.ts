ac3ae41ab9c167eec2260b183cf8b300af22521a
Duan Yihan <69476139+IanDxSSXX@users.noreply.github.com>
2022-10-19 11:19:50 +0800

    add observe

--- src/core/index.ts
+++ src/core/index.ts
@@ -6,10 +6,10 @@ import {TagView, ElementView, FragmentView} from "./utils/RUIWrapper";
 import {ViewWrapper, View, FuncView} from "./element/RUIView"
 import {State, Ref, Spring, Theme, Navigate} from "./element/HookDecorator";
 import ContextProvider from "./context/ContextProvider";
-import {Prop, DotProp, Context, Callback, Contexts, Hook, SHook} from "./element/Decorator";
+import {Prop, DotProp, Context, Callback, Contexts, Hook, SHook, Observe} from "./element/Decorator";
 import ForEach from "./control/ForEach";
 
 export {useRUIState, useTrigger, useTriggerEffect}  // hooks
 export {FragmentView, NavigationView, ConditionView, ThemeProvider, ContextProvider, ForEach}  //  view
 export {View, ViewWrapper, FuncView, TagView, ElementView}  // view wrapper
-export {Prop, DotProp, Context, Contexts, Callback, State, Ref, Spring, Theme, Navigate, Hook, SHook}  // decorators
+export {Prop, DotProp, Context, Contexts, Callback, State, Ref, Spring, Theme, Navigate, Hook, SHook, Observe}  // decorators
